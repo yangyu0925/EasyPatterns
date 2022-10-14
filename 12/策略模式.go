e0fb02ccbab6602a72e6a0a985604df3622dbceb
yangyu <yangyu@delant.com.cn>
2022-10-14 11:32:47 +0800

    first commit

--- 12/策略模式.go
+++ 12/策略模式.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+//type WeaponStrategy interface {
+//	UseWeapon()
+//}
+//
+//type Ak47 struct {}
+//
+//func (ak *Ak47) UseWeapon()  {
+//	fmt.Println("使用Ak47 去战斗")
+//}
+//
+//type Knifr struct {}
+//
+//func (ak *Knifr) UseWeapon() {
+//	fmt.Println("使用匕首 去战斗")
+//}
+//
+//type Hero struct {
+//	strategy WeaponStrategy
+//}
+//
+//func (h *Hero) SetWeaponStgrategy(s WeaponStrategy)  {
+//	h.strategy = s
+//}
+//
+//func (h *Hero) Fight()  {
+//	h.strategy.UseWeapon()
+//}
+//
+//func main() {
+//	hero := Hero{}
+//
+//	hero.SetWeaponStgrategy(new(Ak47))
+//	hero.Fight()
+//
+//	hero.SetWeaponStgrategy(new(Knifr))
+//	hero.Fight()
+//}
+
 type SellStrategy interface {
 	GetPrice(price float64) float64
 }
@@ -10,13 +48,12 @@ type StrategyA struct {}
 
 func (sa *StrategyA) GetPrice(price float64) float64 {
 	fmt.Println("执行策略A, 所有商品打八折")
-
 	return price * 0.8
 }
 
 type StrategyB struct {}
 
-func (sb *StrategyB) GetPrice(price float64) float64 {
+func (sa *StrategyB) GetPrice(price float64) float64 {
 	fmt.Println("执行策略B, 所有商品满200 减100")
 
 	if price >= 200 {
@@ -31,7 +68,7 @@ type Goods struct {
 	Strategy SellStrategy
 }
 
-func (g *Goods) SetStrateGy(s SellStrategy)  {
+func (g *Goods) SetStrategy(s SellStrategy)  {
 	g.Strategy = s
 }
 
@@ -42,13 +79,13 @@ func (g *Goods) SellPrice() float64 {
 
 func main() {
 	nike := Goods{
-		Price: 200.0,
+		Price: 200.00,
 	}
 
-	nike.SetStrateGy(new(StrategyA))
-	fmt.Println("上午nike鞋卖", nike.SellPrice())
+	nike.SetStrategy(new(StrategyA))
+	fmt.Println("上午", nike.SellPrice())
 
-	nike.SetStrateGy(new(StrategyB))
-	fmt.Println("下午nike鞋卖", nike.SellPrice())
+	nike.SetStrategy(new(StrategyB))
+	fmt.Println("下午", nike.SellPrice())
 
 }
